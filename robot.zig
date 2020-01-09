745de599cc3113cbe2ac9ac8a653b4c44a70d228
Matthew Phillips <phillipsm2@gmail.com>
2020-01-09 07:37:36 -0500

    Checking multiple transitions

--- robot.zig
+++ robot.zig
@@ -1,8 +1,13 @@
 const std = @import("std");
 
+fn defaultGuard() bool {
+  return true;
+}
+
 pub const Transition = struct {
   from: []const u8,
-  to: []const u8
+  to: []const u8,
+  guard_fn: fn () bool = defaultGuard,
 };
 
 pub const State = struct {
@@ -19,11 +24,31 @@ pub const Event = struct {
   name: []const u8
 };
 
+fn LinkedList(comptime T: type) type {
+  return struct {
+    pub const Node = struct {
+      prev: ?*Node,
+      next: ?*Node,
+      data: T,
+    };
+
+    first: ?*Node,
+    last:  ?*Node,
+    len:   usize,
+  };
+}
+
+const ListOfTransitions = LinkedList(Transition);
+
 pub fn transition(from: []const u8, to: []const u8) Transition {
   return Transition{
     .from = from,
     .to = to
   };
+}
+
+pub fn addGuard(t: *Transition, comptime gfn: fn () bool) void {
+  t.guard_fn = gfn;
 }
 
 pub fn state(name: []const u8, transitions: []const Transition) State {
@@ -42,12 +67,24 @@ pub fn createMachine(states: []const State) Machine {
   };
 }
 
-fn transitionTo(machine: Machine, current: State, t: Transition, ev: Event) State {
-  // TODO run guards
+fn transitionTo(machine: Machine, current: State, candidates: ListOfTransitions, ev: Event) State {
+  // Run guards
+  var it = candidates.first;
 
-  for (machine.states) |s| {
-    if(std.mem.eql(u8, s.name, t.to)) {
-      return s;
+  while (it) |node| : (it = node.next) {
+    var t = node.data;
+
+    
+
+    var guard_passed = t.guard_fn();
+    if(!guard_passed) {
+      continue;
+    }
+
+    for (machine.states) |s| {
+      if(std.mem.eql(u8, s.name, t.to)) {
+        return s;
+      }
     }
   }
 
@@ -58,11 +95,31 @@ pub fn send(machine: Machine, current: State, ev: Event) State {
   const transitions = current.transitions;
   var new_state = current;
 
+  var candidates = ListOfTransitions{
+    .first = null,
+    .last = null,
+    .len = 0
+  };
+
+  var i: usize = 0;
+  var node: ListOfTransitions.Node = undefined;
   for (transitions) |t| {
     if(std.mem.eql(u8, t.from, ev.name)) {
-      return transitionTo(machine, current, t, ev);
+      i += 1;
+      var last = node;
+      node = ListOfTransitions.Node{
+        .prev = &last,
+        .next = null,
+        .data = t
+      };
+
+      candidates.last = &node;
+      candidates.len = i;
+      if(candidates.first == null) {
+        candidates.first = &node;
+      }
     }
   }
-  
-  return new_state;
+
+  return transitionTo(machine, current, candidates, ev);
 }
--- test.zig
+++ test.zig
@@ -5,11 +5,12 @@ const robot = @import("robot.zig");
 const State = robot.State;
 const Transition = robot.Transition;
 const createMachine = robot.createMachine;
+const addGuard = robot.addGuard;
 
 test "Can do basic transitions" {
   const states = [_]State {
     robot.state("one", &[_]Transition {
-      .{ .from = "next", .to = "two" }
+      robot.transition("next", "two")
     }),
     robot.state("two", &[_]Transition {})
   };
@@ -20,4 +21,27 @@ test "Can do basic transitions" {
   state = robot.send(machine, state, .{ .name = "next" });
 
   expectEqual(state.name, "two");
+}
+
+fn returnFalse() bool {
+  return false;
+}
+
+test "A guard can prevent a transition" {
+  var transition = robot.transition("next", "two");
+  addGuard(&transition, returnFalse);
+
+  const states = [_] State {
+    robot.state("one", &[_]Transition {
+      transition
+    }),
+    robot.state("two", &[_]Transition {})
+  };
+
+  const machine = createMachine(&states);
+  var state = machine.initial;
+
+  state = robot.send(machine, state, .{ .name = "next" });
+
+  expectEqual(state.name, "one");
 }
